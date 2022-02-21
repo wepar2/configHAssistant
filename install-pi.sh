5e5da9f313e0f3e1c49a7a950871d4d65d50e47a
wepar2 <83143626+wepar2@users.noreply.github.com>
2022-02-21 22:52:28 +0100

    Update Script

--- install-pi.sh
+++ install-pi.sh
@@ -20,29 +20,46 @@ else
 
 	sleep 5
 
+	info "instalando dependencias"
+	sleep 5
+
+	sudo apt-get install \
+	jq \
+	wget \
+	curl \
+	udisks2 \
+	libglib2.0-bin \
+	network-manager \
+	software-properties-common \
+	apparmor-utils \
+	avahi-daemon \
+	dbus \
+	jq \
+	network-manager \
+	libgles-dev \
+	libegl-dev \
+	dbus -y
+
+	sudo ln -s /usr/lib/arm-linux-gnueabihf/libGLESv2.so /usr/lib/libbrcmGLESv2.so
+	sudo ln -s /usr/lib/arm-linux-gnueabihf/libEGL.so /usr/lib/libbrcmEGL.so
+	
+
 	info "Creando carpetas...."
 
 	# creacion carpetas donde guardar datos 
 	sudo mkdir /docker
-	sudo chown pi /docker
-	mkdir /docker/nodered
-	mkdir /docker/nodered/data
-	mkdir /docker/influxdb
-	mkdir /docker/influxdb/var/
-	mkdir /docker/influxdb/var/lib/
-	mkdir /docker/influxdb/var/lib/influxdb
-	mkdir /docker/influxdb/etc/
-	mkdir /docker/influxdb/etc/influxdb
-	mkdir /docker/portainer
-	mkdir /home/pi/borrame
+	sudo mkdir /docker/nodered
+	sudo mkdir /docker/nodered/data
+	sudo mkdir /docker/influxdb
+	sudo mkdir /docker/influxdb/var/
+	sudo mkdir /docker/influxdb/var/lib/
+	sudo mkdir /docker/influxdb/var/lib/influxdb
+	sudo mkdir /docker/influxdb/etc/
+	sudo mkdir /docker/influxdb/etc/influxdb
+	sudo mkdir /docker/portainer
+	sudo chown pi -R /docker
 	mkdir /home/pi/backupDocker
-
-	cp /home/pi/installHassio.sh /home/pi/borrame
-
-	rm -rf installHassio.sh
-
-	cd borrame
-	chmod 777 installHassio.sh
+	sudo chown pi /home/pi/backupDocker
 	
 	info "instalando AnyDesk"
 	wget https://download.anydesk.com/rpi/anydesk_6.1.1-1_armhf.deb
@@ -66,25 +83,6 @@ else
 
 	sleep 5
 	sudo docker run --name nodered -itd --restart=always -p 1880:1880 -v /docker/nodered/data:/data  nodered/node-red && sudo docker run --name influxdb -itd --restart=always -p 8086:8086 -p 8083:8083 -p 2003:2003 -v /docker/influxdb/var/lib/influxdb:/var/lib/influxdb -v /docker/influxdb/etc/influxdb:/etc/influxdb influxdb:1.8
-
-
-	info "instalando dependencias"
-	sleep 5
-
-	sudo apt-get install \
-	jq \
-	wget \
-	curl \
-	udisks2 \
-	libglib2.0-bin \
-	network-manager \
-	software-properties-common \
-	apparmor-utils \
-	avahi-daemon \
-	dbus \
-	jq \
-	network-manager \
-	dbus -y
 
 	sleep 1
 	sudo pip install mega.py -y
@@ -118,5 +116,5 @@ else
 
 	sleep 10
 	info "Instalando home-assistant"
-	sudo ./installHassio.sh  -m raspberrypi4 -d /docker/hassio/
+	info "sudo ./installHassio.sh  -m raspberrypi4 -d /docker/hassio/"
 fi
