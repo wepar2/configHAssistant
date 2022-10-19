8c7d7a337816349fa979b01a8be7e9a9cedb665d
wepar2 <83143626+wepar2@users.noreply.github.com>
2022-10-19 22:21:03 +0200

    Update install-pi.sh

--- install-pi.sh
+++ install-pi.sh
@@ -35,16 +35,6 @@ else
 	avahi-daemon \
 	dbus \
 	jq \
-	network-manager \
-	libgles-dev \
-	libegl-dev \
-	libminizip1 \
-	libegl1-mesa \
-	dbus -y
-
-	sudo ln -s /usr/lib/arm-linux-gnueabihf/libGLESv2.so /usr/lib/libbrcmGLESv2.so
-	sudo ln -s /usr/lib/arm-linux-gnueabihf/libEGL.so /usr/lib/libbrcmEGL.so
-	
 
 	info "Creando carpetas...."
 
@@ -60,18 +50,8 @@ else
 	sudo mkdir /docker/influxdb/etc/influxdb
 	sudo mkdir /docker/portainer
 	sudo chown pi -R /docker
-	mkdir /home/pi/backupDocker
-	sudo chown pi /home/pi/backupDocker
-	sudo chown pi -R /docker
 	
-	info "instalando AnyDesk"
-	wget https://download.anydesk.com/rpi/anydesk_6.1.1-1_armhf.deb
-
 	sleep 2
-	
-	sudo dpkg -i anydesk_6.1.1-1_armhf.deb
-
-	echo toor1234 | sudo anydesk --set-password
 
 	info "Instalando docker"
 	sleep 5
@@ -86,12 +66,6 @@ else
 	#
 	sudo docker run -itd -p 8000:8000 -p 9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v /docker/portainer:/data portainer/portainer-ce
 
-	sleep 5
-	sudo docker run --name nodered -itd --restart=always -p 1880:1880 -v /docker/nodered/data:/data  nodered/node-red && sudo docker run --name influxdb -itd --restart=always -p 8086:8086 -p 8083:8083 -p 2003:2003 -v /docker/influxdb/var/lib/influxdb:/var/lib/influxdb -v /docker/influxdb/etc/influxdb:/etc/influxdb influxdb:1.8
-
-	sleep 1
-	sudo pip install mega.py
-
 	info "Descargando os-agent"
 	sleep 5
 
@@ -100,24 +74,6 @@ else
 	sleep 5
 	info "instalando os-agent"
 	sudo dpkg -i os-agent_1.2.2_linux_armv7.deb
-
-	info "disable ModemManager"
-	sleep 5
-
-	if systemctl is-enabled ModemManager.service &> /dev/null; then
-	    sudo systemctl disable ModemManager.service && sudo systemctl stop ModemManager.service
-	fi
-
-	sleep 5
-
-	FILE_DOCKER_CONF="/etc/docker/daemon.json"
-
-	URL_RAW_BASE="https://raw.githubusercontent.com/home-assistant/supervised-installer/main/homeassistant-supervised"
-	URL_DOCKER_DAEMON="${URL_RAW_BASE}/etc/docker/daemon.json" 
-
-	URL_NM_CONF="${URL_RAW_BASE}/etc/docker/daemon.json"
-
-	sudo curl -sL ${URL_DOCKER_DAEMON} > "${FILE_DOCKER_CONF}"
 
 	sleep 10
 	info "Instalando home-assistant"
